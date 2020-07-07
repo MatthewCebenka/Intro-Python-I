35d15c2b840e823d13e52b6e0bd0ea8900d1c678
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-07-07 18:57:38 -0400

    lists

--- src/05_lists.py
+++ src/05_lists.py
@@ -13,8 +13,8 @@ print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-#for item in y:
-#    x.append(item)
+for item in y:
+    x.append(item)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
