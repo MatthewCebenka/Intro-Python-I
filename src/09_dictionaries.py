77a6cee17c9ca9c591c38c589f0be3e1f6137f83
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-07-08 17:32:18 -0400

    dict

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -37,7 +37,7 @@ waypoints = [
 # YOUR CODE HERE
 
 waypoints.append({"lat": 44, "lon": 121, "name": "my place"})
-
+print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
