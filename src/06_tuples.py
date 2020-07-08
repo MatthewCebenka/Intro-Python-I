f0302fbc20026c0bca1f79864db19fcf3d50d5ae
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-07-08 13:11:10 -0400

    tuples

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -44,5 +44,5 @@ def print_tuple(t):
 print_tuple(t)
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
-#print_tuple(u)
+u = (1,)  # What needs to be added to make this work?
+print_tuple(u)
